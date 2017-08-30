336f90470df24b643249a1978954f0deae734303
Joey Morris <rjmorris12@gmail.com>
2017-08-30 00:35:53 -0400

    Revert "Remove unnecessary condition"
    Turns out it was necessary after all. Otherwise, forfeits aren't displayed in the list of game results.
    
    This reverts commit f95c5493b4d91d9baf3aa58b9defe6b1e13e6bd8.

--- js/results.js
+++ js/results.js
@@ -193,7 +193,7 @@ q.await(function(error, games, teams) {
         .classed('col-xs-12', true)
         .classed('col-sm-3', true)
         .html(function(d) {
-            if (d.played) {
+            if (d.played || d.forfeit) {
                 var result = '';
                 if (d.win) {
                     result += 'W, ';
